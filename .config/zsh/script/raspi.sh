f83340de65614a2dbf8792129400e321b01d5735
Pasi Bergman <pasi.bergman@iki.fi>
2021-01-19 13:34:22 +0200

    Add raspi aliases

--- .config/zsh/script/raspi.sh
+++ .config/zsh/script/raspi.sh
@@ -5,8 +5,15 @@
 # sudo nvim /etc/netctl/wireless-wpa
 #
 # Start/Stop Wi-Fi now
-alias wifi-start="sudo netctl start wireless-wpa"
-alias wifi-stop="sudo netctl stop wireless-wpa"
-# Start/Stop network after boot
-alias wifi-enable="sudo netctl enable wireless-wpa"
-alias wifi-disable="sudo netctl disable wireless-wpa"
+
+if [[ "$(uname -a)" == *"raspi"* ]]; then
+    alias wifi-start="sudo netctl start wireless-wpa"
+    alias wifi-stop="sudo netctl stop wireless-wpa"
+    # Start/Stop network after boot
+    alias wifi-enable="sudo netctl enable wireless-wpa"
+    alias wifi-disable="sudo netctl disable wireless-wpa"
+
+    alias ytop=bpytop
+    alias top=bpytop
+fi
+
