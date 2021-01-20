c1a4ffbce306898c96f36322543b1bf6c98d96c5
Pasi Bergman <pasi.bergman@iki.fi>
2021-01-20 20:07:02 +0000

    Show CPU temp and freq only if temp is >50C

--- .config/zsh/script/cpu-info.sh
+++ .config/zsh/script/cpu-info.sh
@@ -7,11 +7,15 @@ if [[ "$(uname -a)" != *"raspi"* ]]; then
     return
 fi
 
+echo "Running cpu-info:"
+
 while [ true ]
 do
     NOW=$(date +"%Y-%m-%d %T")
     cpu_temp=$(head -n 1 /sys/class/thermal/thermal_zone0/temp)
     cpu_freq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
-    printf "%s: CPU frequency %4.0f MHz, temperature %2.0f°C\n" "$NOW"  $(( $cpu_freq/1000 )) $(( $cpu_temp/1000 ))
+    if (( cpu_temp > 50000 )); then
+        printf "%s: CPU frequency %4.0f MHz, temperature %2.0f°C\n" "$NOW"  $(( $cpu_freq/1000 )) $(( $cpu_temp/1000 ))
+    fi;
     sleep 5s
 done
