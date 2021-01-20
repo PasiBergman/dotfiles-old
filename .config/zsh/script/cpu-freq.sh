6a32b35743ad378eee7db7f78326637b33971139
Pasi Bergman <pasi.bergman@iki.fi>
2021-01-20 12:05:41 +0200

    Add packages function

--- .config/zsh/script/cpu-freq.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-echo ""
-while [ true ]
-do
-    NOW=$(date +"%Y-%m-%d %T")
-    cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq | xargs -I{} awk "BEGIN {printf \"${NOW}: CPU frequency %.0f MHz\n\", {}/1000}"
-    sleep 5s
-done
--- .config/zsh/script/linux.sh
+++ .config/zsh/script/linux.sh
@@ -8,5 +8,43 @@ if [[ "$(uname)" != "Linux" ]]; then return; fi
 
 alias ls='ls --color=auto'
 
+# =============================
+# Functions
+# =============================
+
+packages() {
+    FILENAME=$(echo $HOST | sed -e '/afrodite-//g')
+    case "$HOST" in
+        afrodite-arch)
+            SETUP_FOLDER="$HOME/.config/setup/arch"
+            ;;
+        afrodite-raspi3)
+            SETUP_FOLDER="$HOME/.config/setup/raspi"
+            ;;
+        *)
+            echo "Unknown host. Exiting."
+            return
+            ;;
+    esac
+
+    echo "List of explicit packages from main repository"
+    echo "----------------------------------------------"
+    pacman -Qqn | tee "${SETUP_FOLDER}/${FILENAME}-pacman-Qqn.txt"
+    echo ""
+    echo "List of explicit packages from AUR"
+    echo "----------------------------------------------"
+    pacman -Qqm | tee "${SETUP_FOLDER}/${FILENAME}-pacman-Qqm.txt"
+    echo ""
+    echo "# List of unneeded dependencies (orphans)"
+    echo "# ----------------------------------------------"
+    pacman -Qdt | tee "${SETUP_FOLDER}/${FILENAME}-pacman-Qdt.txt"
+}
+
+
+# =============================
+# Sources
+# =============================
+
 # Load Raspberry Pi specific file
 [ -f $HOME/.config/zsh/script/raspi.sh ] && source $HOME/.config/zsh/script/raspi.sh
+
