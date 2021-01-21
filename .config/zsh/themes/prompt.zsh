9ab0d8ffccdc695b40a7007957e986dc70bb908a
Pasi Bergman <pasi.bergman@iki.fi>
2021-01-21 19:48:57 +0200

    Add missing function

--- .config/zsh/themes/prompt.zsh
+++ .config/zsh/themes/prompt.zsh
@@ -5,26 +5,18 @@ precmd() {
 }
 setopt PROMPT_SUBST
 
+git_folder () {
+    git rev-parse --git-dir >/dev/null 2>/dev/null
+    return $?
+}
+
 get_prompt_char () {
     git_folder && echo '±' && return
-    hg root >/dev/null 2>/dev/null && echo 'Hg' && return
     echo 'o'
 }
 
 get_virtualenv_info () {
     [ $VIRTUAL_ENV ] && echo '('`basename $VIRTUAL_ENV`') '
-}
-
-get_git_prompt () {
-    if git_folder; then
-        local branch=$(git_current_branch)
-        local change_count=$(git_dirty_count)
-        local git_change
-        if [[  $change_count -gt 0 ]]; then
-            git_change=`echo " %{%F{red}%}  ${change_count}%{%f%{%f%}%}"`
-        fi
-        echo " %{%F{yellow}%} $branch$git_change%{%f%}"
-    fi
 }
 
 _blue="%{$fg[blue]%}"
