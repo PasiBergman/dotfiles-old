026163d2ec3518eea280e736d4ee3499d5c8a401
Pasi Bergman <pasi.bergman@iki.fi>
2021-01-28 20:53:54 +0200

    Changed tmux project aliases. Add prompt things: sudo exit code

--- .config/zsh/themes/prompt.zsh
+++ .config/zsh/themes/prompt.zsh
@@ -1,23 +1,10 @@
+
 autoload -Uz vcs_info
 zstyle ':vcs_info:*' enable git
 precmd() {
     vcs_info
 }
 setopt PROMPT_SUBST
-
-git_folder () {
-    git rev-parse --git-dir >/dev/null 2>/dev/null
-    return $?
-}
-
-get_prompt_char () {
-    git_folder && echo '±' && return
-    echo 'o'
-}
-
-get_virtualenv_info () {
-    [ $VIRTUAL_ENV ] && echo '('`basename $VIRTUAL_ENV`') '
-}
 
 _blue="%{$fg[blue]%}"
 _green="%{$fg[green]%}"
@@ -28,10 +15,42 @@ _color_reset="%{%f%}"
 _bold="%{%B%}"
 _bold_reset="%{%b%}"
 
+# Show [sudo] in prompt if there is an active sudo session
+is_sudo() {
+    SUDO_SESSION=$(sudo -n uptime 2>&1 | grep "load" | wc -l)
+    if [ ${SUDO_SESSION} -gt 0 ]; then
+        echo "${_red}[sudo]${_color_reset} "
+    fi
+}
+
+# Show exit code of the last command if there was an error 
+# i.e. exit code > 0
+last_status_code() {
+    LAST_EXIT=${?}
+    if [ ${LAST_EXIT} -gt 0 ]; then
+        echo "${_red}[exit:${LAST_EXIT}]${_color_reset} "
+    fi
+}
+
+git_folder () {
+    git rev-parse --git-dir >/dev/null 2>/dev/null
+    return $?
+}
+
+get_prompt_char () {
+    # git_folder && echo '±' && return
+    echo 'o'
+}
+
+get_virtualenv_info () {
+    [ $VIRTUAL_ENV ] && echo '['`basename $VIRTUAL_ENV`'] '
+}
+
+
 zstyle ':vcs_info:git*' formats " on ${_yellow} %b ${_red}  %u%c%m${_color_reset}"
 zstyle ':vcs_info:git*' actionformats "%s  %r/%S %b (%a) %m%u%c "
 
 export PS1='
 ${_blue}%n%\@%m${cend} in ${_green}${_bold}%~${_bold_reset}${_color_reset}${vcs_info_msg_0_}
-$(get_virtualenv_info)$(get_prompt_char) '
+$(last_status_code)$(is_sudo)$(get_virtualenv_info)$(get_prompt_char) '
 
